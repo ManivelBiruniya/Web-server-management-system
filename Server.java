92632293c0b8d851c819f4902b269b4b01e8ee49
Jainduxd <manojranjanarush@gmail.com>
2026-06-22 21:00:19 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,36 @@
+
+public abstract class Server {
+
+    protected String serverName;
+    protected String status;   // "RUNNING" or "STOPPED"
+    protected long uptimeSeconds;
+
+    public Server(String serverName) {
+        this.serverName = serverName;
+        this.status = "STOPPED";
+        this.uptimeSeconds = 0;
+    }
+
+    
+    public abstract void start();
+    public abstract void stop();
+    public abstract void restart();
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public long getUptime() {
+        return uptimeSeconds;
+    }
+
+    
+    public void healthCheck() {
+        System.out.println("[HEALTH CHECK] " + serverName + " -> Status: " + status
+                + " | Uptime: " + uptimeSeconds + "s");
+    }
+}
