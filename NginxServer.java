28b7b3ed7d1ee2db8a24b51a3643fbd43a2a81fc
fathimaashrifa08-creator <fathima.ashrifa08@gmail.com>
2026-06-22 22:52:20 +0530

    Add NginxServer.java

---
+++ NginxServer.java
@@ -0,0 +1,27 @@
+
+public class NginxServer extends Server {
+
+    public NginxServer() {
+        super("Nginx Server");
+    }
+
+    @Override
+    public void start() {
+        status = "RUNNING";
+        uptimeSeconds = 0;
+        System.out.println("[NGINX] Server started successfully (event-driven mode).");
+    }
+
+    @Override
+    public void stop() {
+        status = "STOPPED";
+        System.out.println("[NGINX] Server stopped.");
+    }
+
+    @Override
+    public void restart() {
+        System.out.println("[NGINX] Restarting...");
+        stop();
+        start();
+    }
+}
